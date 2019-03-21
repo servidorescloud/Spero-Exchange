a1f01a4ba6e3d1422cffe1dadf3e5a4d82b06995
SperoCoin <helper@sperocoin.org>
2019-03-20 21:26:28 -0300

    63 - Atualização de Dividendos e arquivos(Site em Manutenção)

--- config/maintenance.sh
+++ config/maintenance.sh
@@ -2,10 +2,13 @@
 
 if [ "$1" == "off" ]; then
   echo "switch off maintenance mode"
-  ln -sf /home/deploy/peatio/shared/config/nginx.conf /etc/nginx/conf.d/peatio.conf
+  ln -sf /home/spero/peatio/config/nginx.conf /etc/nginx/conf.d/peatio.conf
+  mv /home/spero/peatio/public/404.html.old /home/spero/peatio/public/404.html
   service nginx reload
 else
   echo "switch on maintenance mode"
-  ln -sf /home/deploy/peatio/shared/config/nginx_maintenance.conf /etc/nginx/conf.d/peatio.conf
+  ln -sf /home/spero/peatio/config/nginx_maintenance.conf /etc/nginx/conf.d/peatio.conf
+  mv //home/spero/peatio/public/404.html /home/spero/peatio/public/404.html.old
+  cp /home/spero/peatio/public/503.html /home/spero/peatio/public/404.html
   service nginx reload
 fi
