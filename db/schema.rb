41802ce0e19a1c16d873e0eea027d03ff666a800
SperoCoin <helper@sperocoin.org>
2019-03-21 12:27:26 -0300

    65 - Sistema de Listagem de Moedas Adicionado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413201520) do
+ActiveRecord::Schema.define(version: 20180413201523) do
 
   create_table "account_versions", force: true do |t|
     t.integer  "member_id"
@@ -237,6 +237,33 @@ ActiveRecord::Schema.define(version: 20180413201520) do
   end
 
   add_index "intraday_dividends", ["dividend_id", "created_at"], name: "index_intraday_dividends_on_dividend_id_and_created_at", using: :btree
+
+  create_table "listing_requests", force: true do |t|
+    t.string   "ticker"
+    t.string   "name"
+    t.string   "algo"
+    t.string   "codebase"
+    t.string   "source"
+    t.string   "be"
+    t.string   "home"
+    t.string   "btt"
+    t.string   "lang"
+    t.string   "email"
+    t.string   "discord"
+    t.string   "twitter"
+    t.string   "reddit"
+    t.string   "facebook"
+    t.string   "telegram"
+    t.text     "comment"
+    t.string   "secret"
+    t.string   "address"
+    t.decimal  "amount",     precision: 32, scale: 16
+    t.string   "aasm_state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "listing_requests", ["amount", "created_at"], name: "index_listing_requests_on_amount_and_created_at", using: :btree
 
   create_table "members", force: true do |t|
     t.string   "sn"
